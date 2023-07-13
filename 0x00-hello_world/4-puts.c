a414b4467db69ef2376c3ba1a5e91552d996a3e0
OnyisoChris <onyisochris3@gmail.com>
2023-07-13 16:45:05 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * main - Return point
- *
- * Return: Always 0 (Success)
- */
+* main - Return point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
