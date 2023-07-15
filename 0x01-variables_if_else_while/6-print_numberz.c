1d36a66476e661e11d91cf4c4b2c6e0cd0517cde
OnyisoChris <onyisochris3@gmail.com>
2023-07-15 15:23:24 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,14 @@
  * Description:program that prints all single digit numbers of b10 from 0
  * Return: Always 0
  *
- * */
+ **/
 
-int main() {
+int main(void) 
+{
 	int num;
 
-	for (num = 0; num <= 9; num++) {
+	for (num = 0; num <= 9; num++) 
+	{
 		putchar(num +'0');
 	}
 
