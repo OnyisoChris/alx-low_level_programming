d8009917d7490c11c77beb1778bf880b3df2f772
OnyisoChris <onyisochris3@gmail.com>
2023-07-19 07:34:12 -0700

    Prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int k, l;
+
+	for (k = 0; k < 24; k++)
+	{
+		for (l = 0; l < 60; l++)
+		{
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+			_putchar(':');
+			_putchar(l / 10 + '0');
+			_putchar(l % 10 + '0');
+		}
+	}
+}
