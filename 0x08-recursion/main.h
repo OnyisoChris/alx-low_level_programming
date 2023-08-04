687058c74cfccdc7db56d95ea15b7a2fbda70458
OnyisoChris <onyisochris3@gmail.com>
2023-08-03 20:01:28 -0700

    Square root of a number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt(int n, int j);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int _prime(int n, int j);
 
 #endif
