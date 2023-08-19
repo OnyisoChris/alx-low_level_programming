ccafa97cb4b3aed103d98c73a60596cee46454a5
OnyisoChris <onyisochris3@gmail.com>
2023-08-19 15:36:53 -0700

    Frees memory

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
