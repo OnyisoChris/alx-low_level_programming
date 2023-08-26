d92cdf7290f396c9cf1d19a0f714a721d0b0d1bb
OnyisoChris <onyisochris3@gmail.com>
2023-08-26 10:04:56 -0700

    Pints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ *
+ * Return:
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int j;
+
+	va_start(args, n);
+	
+	for (j = 0; j < n; j++)
+	{
+		int num = va_arg(args, int);
+
+		printf("%d", num);
+
+		if (j != n - 1)
+		{
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+
+		printf("\n");
+		va_end(args);
+	}
+}
