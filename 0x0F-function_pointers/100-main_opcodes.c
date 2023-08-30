9ec04b6786da757f178e2c9eb1bbcb3f97b8f932
OnyisoChris <onyisochris3@gmail.com>
2023-08-30 10:01:30 -0700

    Opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,47 +1,42 @@
 #include "function_pointers.h"
 
 /**
- * _opcodes - prints the opcodes in hexadecimal format
- * @begin: starting address of opcodes
- * @length: number of opcodes to be printed
- */
-
-void _opcodes(const unsigned char *begin, size_t length)
-{
-	size_t j;
-
-	for (j = 0; j < length; j++)
-	{
-		printf("%02x ", begin[j]);
-	}
-	printf("\n");
-}
-
-/**
  * main - Entry point
  * @argc: number of arguments
  * @argv: array of argument strings
+ *
  * Return: 0 on success, 1 for incorrect arguments, 2 for -ve bytes
  */
 
 int main(int argc, char *argv[])
 {
+	int n_bytes, j;
+	char *arr;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
-	int n_bytes = atoi(argv[1]);
+	n_bytes = atoi(argv[1]);
 
 	if (n_bytes <= 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
-	unsigned char *main_start = (unsigned char *)&main;
 
-	_opcodes(main_start, n_bytes);
+	arr = (char *)main;
 
+	for (j = 0; j < n_bytes; j++)
+	{
+		if (j == n_bytes - 1)
+		{
+			printf("%02hhx\n", arr[j]);
+			break;
+		}
+		printf("%02hhx ", arr[j]);
+	}
 	return (0);
 }
